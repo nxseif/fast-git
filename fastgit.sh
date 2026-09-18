e2de4939e63e7003d9e1fe2ba8029693ed261739
nxseif <seifeddinekhfifi@gmail.com>
2026-09-18 22:58:19 +0100

    Update fastgit.sh

--- fastgit.sh
+++ fastgit.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-FILE=$1
+fle="$1"
 
-if [ -z "$FILE" ]
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1
 then
-    echo "Usage: ./fastgit.sh filename"
+    echo "warning youre not inside git repo!!"
     exit 1
 fi
 
-if [ ! -f "$FILE" ]
+if [ -z "$fle" ]
 then
-    echo "error: file does not exist."
+    echo "usage: fastgit filename"
     exit 1
 fi
 
-git add "$FILE"
-git commit -m "Update $FILE"
-git push
+if [ ! -f "$fle" ]
+then
+    echo "warning: file does not exist"
+    exit 1
+fi
+
+if ! git add "$fle"
+then
+    echo "warning: git add fail"
+    exit 1
+fi
+
+if ! git commit -m "Update $fle"
+then
+    echo "warning:commit fail"
+    exit 1
+fi
+
+if ! git push
+then
+    echo "warining add failed"
+    exit 1
+fi
+
+echo "done"
